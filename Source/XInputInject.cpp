018c9c19a8f4c752feb31cacf851f6f2779f63c5
Lesueur Benjamin <benjamin.lesueur@live.com>
2023-07-17 14:38:54 +0200

    improve code
    - use console output
    - implement setstate
    - improve getstate loop

--- Source/XInputInject.cpp
+++ Source/XInputInject.cpp
@@ -1,5 +1,5 @@
 ﻿#include <Windows.h>
-//#include <Xinput.h>
+#include <Xinput.h>
 #include "MinHook.h"
 #include <fstream>
 
@@ -10,51 +10,13 @@
 #endif
 
 #pragma comment(lib, "xinput.lib")
+#pragma warning(disable:4996)
 
-#define XINPUT_GAMEPAD_DPAD_UP          0x0001
-#define XINPUT_GAMEPAD_DPAD_DOWN        0x0002
-#define XINPUT_GAMEPAD_DPAD_LEFT        0x0004
-#define XINPUT_GAMEPAD_DPAD_RIGHT       0x0008
-#define XINPUT_GAMEPAD_START            0x0010
-#define XINPUT_GAMEPAD_BACK             0x0020
-#define XINPUT_GAMEPAD_LEFT_THUMB       0x0040
-#define XINPUT_GAMEPAD_RIGHT_THUMB      0x0080
-#define XINPUT_GAMEPAD_LEFT_SHOULDER    0x0100
-#define XINPUT_GAMEPAD_RIGHT_SHOULDER   0x0200
-#define XINPUT_GAMEPAD_A                0x1000
-#define XINPUT_GAMEPAD_B                0x2000
-#define XINPUT_GAMEPAD_X                0x4000
-#define XINPUT_GAMEPAD_Y				0x8000
-
-#define BATTERY_TYPE_DISCONNECTED		0x00
-
-#define XUSER_MAX_COUNT                 4
-#define XUSER_INDEX_ANY					0x000000FF
-
-#define ERROR_DEVICE_NOT_CONNECTED		1167
-#define ERROR_SUCCESS					0
-
-typedef struct _XINPUT_GAMEPAD
-{
-	WORD                                wButtons;
-	BYTE                                bLeftTrigger;
-	BYTE                                bRightTrigger;
-	SHORT                               sThumbLX;
-	SHORT                               sThumbLY;
-	SHORT                               sThumbRX;
-	SHORT                               sThumbRY;
-} XINPUT_GAMEPAD, *PXINPUT_GAMEPAD;
-
-typedef struct _XINPUT_STATE
-{
-	DWORD                               dwPacketNumber;
-	XINPUT_GAMEPAD                      Gamepad;
-} XINPUT_STATE, *PXINPUT_STATE;
-
-
-typedef DWORD(WINAPI *XINPUTGETSTATE)(DWORD, XINPUT_STATE*);
+typedef DWORD(WINAPI* XINPUTSETSTATE)(DWORD, XINPUT_VIBRATION*);
+typedef DWORD(WINAPI* XINPUTGETSTATE)(DWORD, XINPUT_STATE*);
 
 // Pointer for calling original
+static XINPUTSETSTATE hookedXInputSetState = nullptr;
 static XINPUTGETSTATE hookedXInputGetState = nullptr;
 
 std::ofstream outfile;
@@ -75,82 +37,137 @@ inline MH_STATUS MH_CreateHookApiEx(LPCWSTR pszModule, LPCSTR pszProcName, LPVOI
 //Own GetState
 DWORD WINAPI detourXInputGetState(DWORD dwUserIndex, XINPUT_STATE* pState)
 {
-	//MessageBox(0, "XinputGetSate", "XINPUT", MB_OK);
-	
-	//ZeroMemory(pState, sizeof(XINPUT_STATE));
-
 	// first call the original function
 	DWORD toReturn = hookedXInputGetState(dwUserIndex, pState);
+	printf("detourXInputGetState %u\n", dwUserIndex);
 	
-	switch (dwUserIndex) {
-		case 0: {
-			XINPUT_STATE pState2;
-			ZeroMemory(&pState2, sizeof(XINPUT_STATE));
-
+	switch (dwUserIndex)
+	{
+		case 0:
+		{
 			// get pState from controller 1...3
-			for (int idx = 1; idx < 4; idx++)
+			for (DWORD idx = 1; idx < XUSER_MAX_COUNT; idx++)
 			{
-				DWORD result = hookedXInputGetState(idx, &pState2);
-				if (result == ERROR_SUCCESS)
+				// Create a XINPUT_STATE structure to store the controller state
+				XINPUT_STATE pState2;
+				ZeroMemory(&pState2, sizeof(XINPUT_STATE));
+				toReturn = hookedXInputGetState(idx, &pState2);
+
+				if (toReturn == ERROR_SUCCESS)
 				{
+					printf("GetState(%u), %u\n", idx, pState2.Gamepad.wButtons);
+
+					// copy inputs from controller
 					pState->Gamepad = pState2.Gamepad;
 					break;
+				}
+				else
+				{
+					printf("GetState(%u) failed\n", idx);
 				}
 			}
 			break;
 		}
-		default: {
-			XINPUT_STATE pStateBlank;
-			ZeroMemory(&pStateBlank, sizeof(XINPUT_STATE));
 
+		default:
+		{
 			// blank pState of other controllers except first controller
-			pState->Gamepad = pStateBlank.Gamepad;
+			ZeroMemory(pState, sizeof(XINPUT_STATE));
 		}
 	}
 		
 	return toReturn;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-	DWORD  ul_reason_for_call,
-	LPVOID lpReserved
-)
+//Own SetState
+DWORD WINAPI detourXInputSetState(DWORD dwUserIndex, XINPUT_VIBRATION* pVibration)
 {
+	// first call the original function
+	DWORD toReturn = hookedXInputSetState(dwUserIndex, pVibration);
+	printf("detourXInputSetState %u\n", dwUserIndex);
+
+	switch (dwUserIndex)
+	{
+		case 0:
+		{
+			// get pState from controller 1...3
+			for (DWORD idx = 1; idx < XUSER_MAX_COUNT; idx++)
+			{
+				toReturn = hookedXInputSetState(idx, pVibration);
+
+				if (toReturn == ERROR_SUCCESS)
+				{
+					printf("SetState(%u), L:%u, R:%u\n", idx, pVibration->wLeftMotorSpeed, pVibration->wRightMotorSpeed);
+					break;
+				}
+				else
+				{
+					printf("SetState(%u) failed\n", idx);
+				}
+			}
+			break;
+		}
+
+		default:
+		{
+			// blank pState of other controllers except first controller
+			pVibration->wLeftMotorSpeed = 0;
+			pVibration->wRightMotorSpeed = 0;
+
+			ZeroMemory(pVibration, sizeof(XINPUT_VIBRATION));
+		}
+	}
+
+	return toReturn;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	AllocConsole();
+	freopen("conin$", "r", stdin);
+	freopen("conout$", "w", stdout);
+	freopen("conout$", "w", stderr);
+
 	switch (ul_reason_for_call){
 		case DLL_PROCESS_ATTACH: {
-			MessageBox(0, "Attached", "XINPUT", MB_OK);
+			printf("Attached\n");
 
 			if (MH_Initialize() == MH_OK)
-				MessageBox(0, "Initialized ok", "XINPUT", MB_OK);
+				printf("Initialized\n");
 
 			//1_4
-			if (hookedXInputGetState == nullptr)
+			if (MH_CreateHookApiEx(L"XINPUT1_4", "XInputSetState", &detourXInputSetState, &hookedXInputSetState) == MH_OK)
 				if (MH_CreateHookApiEx(L"XINPUT1_4", "XInputGetState", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
-					MessageBox(0, "Detour XInputGetState 1_4", "XINPUT", MB_OK);
+					printf("Detour XInputGetState 1_4\n");
 
 			//1_3
 			if (hookedXInputGetState == nullptr)
-				if (MH_CreateHookApiEx(L"XINPUT1_3", "XInputGetState", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
-					MessageBox(0, "Detour XInputGetState 1_3", "XINPUT", MB_OK);
+				if (MH_CreateHookApiEx(L"XINPUT1_3", "XInputSetState", &detourXInputSetState, &hookedXInputSetState) == MH_OK)
+					if (MH_CreateHookApiEx(L"XINPUT1_3", "XInputGetState", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
+						printf("Detour XInputGetState 1_3\n");
 
 			//1_2
 			if (hookedXInputGetState == nullptr)
-				if (MH_CreateHookApiEx(L"XINPUT_1_2", "XInputGetState", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
-					MessageBox(0, "Detour XInputGetState 1_2", "XINPUT", MB_OK);
+				if (MH_CreateHookApiEx(L"XINPUT1_2", "XInputSetState", &detourXInputSetState, &hookedXInputSetState) == MH_OK)
+					if (MH_CreateHookApiEx(L"XINPUT_1_2", "XInputGetState", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
+						printf("Detour XInputGetState 1_2\n");
 
 			//1_1
 			if (hookedXInputGetState == nullptr)
-				if (MH_CreateHookApiEx(L"XINPUT_1_1", "XInputGetState", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
-					MessageBox(0, "Detour XInputGetState 1_1", "XINPUT", MB_OK);
+				if (MH_CreateHookApiEx(L"XINPUT1_1", "XInputSetState", &detourXInputSetState, &hookedXInputSetState) == MH_OK)
+					if (MH_CreateHookApiEx(L"XINPUT_1_1", "XInputGetState", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
+						printf("Detour XInputGetState 1_1\n");
 
 			//1.0
-			if (MH_CreateHookApiEx(L"XINPUT9_1_0", "XInputGetStateEx", &detourXInputGetState, &hookedXInputGetState) == MH_OK) //Ex! - Bulletstorm (2011)
-				MessageBox(0, "Detour XInputGetState 9_1_0", "XINPUT", MB_OK);
+			if (hookedXInputGetState == nullptr)
+				if (MH_CreateHookApiEx(L"XINPUT9_1_0", "XInputSetState", &detourXInputSetState, &hookedXInputSetState) == MH_OK)
+					if (MH_CreateHookApiEx(L"XINPUT9_1_0", "XInputGetStateEx", &detourXInputGetState, &hookedXInputGetState) == MH_OK)
+						printf("Detour XInputGetState 9_1_0\n");
 
 
 			//if (MH_EnableHook(&detourXInputGetState) == MH_OK) //Not working
 			if (MH_EnableHook(MH_ALL_HOOKS) == MH_OK)
-				MessageBox(0, "XInput hooked", "XINPUT", MB_OK);
+				printf("XInput hooked\n");
 
 			break;
 		}
